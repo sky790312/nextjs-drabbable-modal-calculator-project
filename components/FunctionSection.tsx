00769937dc81c4d47118dbcc71801a730effc90f
KevinHu <sky790312yks@gmail.com>
2021-02-24 16:44:40 +0800

    feat: completed function section handle

--- components/FunctionSection.tsx
+++ components/FunctionSection.tsx
@@ -2,15 +2,36 @@ import React from 'react'
 import styled from 'styled-components/'
 import { FlexCenterContainer } from '@/GlobalStyles'
 import { Dispatch } from 'redux'
-import { calculatorActions } from '@/store/state.calculator'
-import { useDispatch } from 'react-redux'
+import { calculatorActions, initialDisplayValue } from '@/store/state.calculator'
+import { useSelector, shallowEqual, useDispatch } from 'react-redux'
+import { RootState } from '@/store/index'
 
-export const FunctionSection: React.FC = () => {
+export const FunctionSection: React.FC = React.memo(() => {
   const dispatch: Dispatch = useDispatch()
-  const { reset } = calculatorActions
+  const { reset, setDisplayValue } = calculatorActions
+  const displayValue = useSelector(
+    (state: RootState) => state.calculator.displayValue,
+    shallowEqual
+  )
 
   const handleReset = () => {
     dispatch(reset())
+  }
+
+  const handleToggleSign = () => {
+    const newDisplayValue = (parseFloat(displayValue) * -1).toString()
+    dispatch(setDisplayValue(newDisplayValue))
+  }
+
+  const handlePercent = () => {
+    if (displayValue === initialDisplayValue) {
+      return
+    }
+
+    const fixedDigits = displayValue.replace(/^-?\d*\.?/, '')
+    const newValue = parseFloat(displayValue) / 100
+    const newDisplayValue = String(newValue.toFixed(fixedDigits.length + 2))
+    dispatch(setDisplayValue(newDisplayValue))
   }
 
   return (
@@ -19,15 +40,15 @@ export const FunctionSection: React.FC = () => {
         <Button onClick={handleReset}>AC</Button>
       </FlexCenterContainer>
       <FlexCenterContainer>
-        <Button>+/-</Button>
+        <Button onClick={handleToggleSign}>+/-</Button>
       </FlexCenterContainer>
       <FlexCenterContainer>
-        <Button>%</Button>
+        <Button onClick={handlePercent}>%</Button>
       </FlexCenterContainer>
     </FunctionSectionContainer>
 
   )
-}
+})
 
 const FunctionSectionContainer = styled.div`
   display: flex;
